c9846decc839db33512b9cea6111e964c0d331f7
vaidehi077 <vaishnavvaidehi03@gmail.com>
2024-03-31 00:04:32 +0530

    file updated

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './TaskCard.css';
 import deleteIcon from "./delete (1).png"
 
-function TaskCard({ task, delFunction, index }) {
+function TaskCard({ title,category, delFunction, index }) {
     return (
         <div className='task-card'> 
-            <h1 className='task-title'>{task}</h1>   
+            <h1 className='task-title'>{title}</h1>
+               <span className='task-category'>{category}</span>
             <img src= {deleteIcon} alt='delete' className='delete-icon' onClick={() => delFunction(index)}/>
         </div>
     );
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,40 +3,61 @@ import './Home.css';
 import addIcon from './add.png';
 import TaskCard from './../../components/TaskCard/TaskCard';
 
-
 function Home() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [error, setError] = useState('');
   const [category, setCategory] = useState('');
 
-  const addTask = () => {
-    if(newTask==''){
-      alert('Please enter a task')
-      return
-    }
-    setTasks([newTask,...tasks ])
-    setNewTask('')
+  const saveTasksToLS = (tasksToSave) => {
+    localStorage.setItem('tasks', JSON.stringify(tasksToSave));
+  }
+
+const validateNewTasks = () =>
+ { if (newTask === '') {
+  setError('Please enter a task');
+  return false;
+} else if (newTask.length < 5) {
+  setError('Task must be at least 5 characters long!');
+  return false;
+} else {
+  setError('');
+  return true;
+}}
+
+  const addTask = () =>
+   {
+   const validationResult = validateNewTasks();
+   if(!validationResult) return;
+
+    const newTasks = [{
+      title: newTask,
+      category: category,
+    },
+    ...tasks
+  ];
+    saveTasksToLS(newTasks);
+    setTasks(newTasks);
+    setNewTask('');
   }
 
   const deleteTask = (index) => {
-    const newTasks = tasks;
-    newTasks.splice(index, 1);
-    setTasks([...newTasks]);
-
-    localStorage.setItem('tasks' , JSON.stringify(newTasks));
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+    saveTasksToLS(newTasks);
   }
 
-  useEffect (() => {
-    if(tasks.length === 0){
-      return
+  useEffect(() => {
+    if (tasks.length === 0) {
+      return;
     }
-    localStorage.setItem('tasks' , JSON.stringify(tasks));
-  }, {tasks})
+    saveTasksToLS(tasks);
+  }, [tasks])
 
   useEffect(() => {
-    const tasks = localStorage.getItem('tasks');
-    if(tasks){
-      setTasks(JSON.parse(tasks));
+    const tasksFromLocalStorage = localStorage.getItem('tasks');
+    if (tasksFromLocalStorage) {
+      setTasks(JSON.parse(tasksFromLocalStorage));
     }
   }, [])
 
@@ -46,31 +67,47 @@ function Home() {
      
       <div className='tasks-container'>
         {
-          tasks.map((task, i) => { return <TaskCard task ={task} key={i} delFunction={deleteTask} index={i}/>})
-        }
-      </div>
-      
-      <div className='input-container'>
-        <input type='text' placeholder='Add a new task' className='task-input' value={newTask}
-       onChange={(e) => {
-        setNewTask(e.target.value)
-       }}
-         />
-         
-        <select className='category-select' value={category} onChange={(e) => setCategory(e.target.value)}> 
-          <option> Category</option>
-          <option value='Study'> 📝 Study</option>
-          <option value='Work'> 👩🏻‍💻 Work</option>
-          <option value=' Personal'> 🧘 Personal</option>
-          <option value='Health'> 💪 Health </option>
-          <option value='Family'>🏡 Family </option>
-          <option value='Social'> 🌐 Social </option>
-          <option value='Errands'> 🛒 Errands </option>
-          <option value='Hobbies'> 🎵 Hobbies</option>
-          <option value='Goals'> 🎯 Goals</option>
-        </select>
 
-        <img src={addIcon} alt='add' className='add-icon' onClick={addTask}/>
+         tasks.map((task, i) => {
+          const {title , category} = task; 
+
+          return (
+          <TaskCard 
+           title={title} 
+           category= {category}
+            key={i} delFunction={deleteTask} 
+            index={i} />)
+         })
+         };
+      </div>
+
+      <p className='error-msg'>{error}</p>
+
+      <div className='input-container'>
+        <input
+          type='text'
+          placeholder='Add a new task'
+          className='task-input'
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <select
+          className='category-select'
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option>Category</option>
+          <option value='Study'>📝 Study</option>
+          <option value='Work'>👩🏻‍💻 Work</option>
+          <option value='Personal'>🧘 Personal</option>
+          <option value='Health'>💪 Health</option>
+          <option value='Family'>🏡 Family</option>
+          <option value='Social'>🌐 Social</option>
+          <option value='Errands'>🛒 Errands</option>
+          <option value='Hobbies'>🎵 Hobbies</option>
+          <option value='Goals'>🎯 Goals</option>
+        </select>
+        <img src={addIcon} alt='add' className='add-icon' onClick={addTask} />
       </div>
     </div>
   );
